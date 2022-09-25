116b1fea3d30ac8478120b9991153210c9ad4466
james-tindal <10291002+james-tindal@users.noreply.github.com>
2022-09-25 01:27:57 +0100

    cmd-UpArrow navigates to ToC

--- public/pagination.js
+++ public/pagination.js
@@ -1,6 +1,8 @@
 const pagination = JSON.parse(document.getElementById('pagination').text)
-document.addEventListener('keydown', e =>
+addEventListener('keydown', e =>
+  e.metaKey && !(e.controlKey || e.altKey || e.shiftKey) &&
+    e.key == 'ArrowUp' && (location = '/') ||
   !(e.controlKey || e.altKey || e.metaKey || e.shiftKey) &&
-  e.key == 'ArrowLeft'  && pagination.previous && (location = pagination.previous) ||
-  e.key == 'ArrowRight' && pagination.next     && (location = pagination.next)
+    e.key == 'ArrowLeft'  && pagination.previous && (location = pagination.previous) ||
+    e.key == 'ArrowRight' && pagination.next     && (location = pagination.next)
 )
